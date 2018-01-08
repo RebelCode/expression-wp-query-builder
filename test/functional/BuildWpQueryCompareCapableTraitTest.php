af57d3686d99acbf494a1c82a7cc7737bef736bd
Miguel Muscat <miguelmuscat93@gmail.com>
2018-01-08 17:55:40 +0100

    Added test for `BuildWpQueryCompareCapableTrait`

---
+++ test/functional/BuildWpQueryCompareCapableTraitTest.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace RebelCode\WordPress\Query\Builder\FuncTest;
+
+use Dhii\Expression\LogicalExpressionInterface;
+use InvalidArgumentException;
+use PHPUnit_Framework_MockObject_MockObject;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see RebelCode\WordPress\Query\Builder\BuildWpQueryCompareCapableTrait}.
+ *
+ * @since [*next-version*]
+ */
+class BuildWpQueryCompareCapableTraitTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\WordPress\Query\Builder\BuildWpQueryCompareCapableTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $methods Optional additional mock methods.
+     *
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    public function createInstance(array $methods = [])
+    {
+        $builder = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+                        ->setMethods(
+                            array_merge(
+                                $methods,
+                                [
+                                    '_isWpQueryCompareExpressionSupported',
+                                    '_getWpQueryCompareKey',
+                                    '_getWpQueryCompareValue',
+                                    '_createInvalidArgumentException',
+                                    '__',
+                                ]
+                            )
+                        );
+
+        $mock = $builder->getMockForTrait();
+        $mock->method('__')->willReturnArgument(0);
+        $mock->method('_createInvalidArgumentException')->willReturnCallback(
+            function($m, $c, $p) {
+                return new InvalidArgumentException($m, $c, $p);
+            }
+        );
+
+        return $mock;
+    }
+
+    /**
+     * Creates a mock logical expression instance.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $type     The expression type.
+     * @param array  $terms    Optional expression terms.
+     * @param bool   $negation Optional negation flag. True to negate the expression, false otherwise.
+     *
+     * @return LogicalExpressionInterface The created mock instance.
+     */
+    public function createLogicalExpression($type, $terms = [], $negation = false)
+    {
+        return $this->mock('Dhii\Expression\LogicalExpressionInterface')
+                    ->getType($type)
+                    ->getTerms($terms)
+                    ->isNegated($negation)
+                    ->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInternalType(
+            'object',
+            $subject,
+            'An instance of the test subject could not be created'
+        );
+    }
+
+    /**
+     * Tests the WP Query comparison build method to ensure that the result contains the information returned by the
+     * abstracted methods.
+     *
+     * @since [*next-version*]
+     */
+    public function testBuildWpQueryCompare()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $expression = $this->createLogicalExpression('EQUAL_TO');
+
+        $subject->expects($this->once())
+                ->method('_isWpQueryCompareExpressionSupported')
+                ->with($expression)
+                ->willReturn(true);
+        $subject->expects($this->once())
+                ->method('_getWpQueryCompareKey')
+                ->with($expression)
+                ->willReturn($key = uniqid('key-'));
+        $subject->expects($this->once())
+                ->method('_getWpQueryCompareValue')
+                ->with($expression)
+                ->willReturn($value = uniqid('value-'));
+
+        $expected = [$key => $value];
+
+        $this->assertEquals(
+            $expected,
+            $reflect->_buildWpQueryCompare($expression),
+            'Expected and retrieved results are not equal'
+        );
+    }
+
+    /**
+     * Tests the WP Query comparison build method to assert whether an exception is thrown if an expression is not
+     * supported.
+     *
+     * @since [*next-version*]
+     */
+    public function testBuildWpQueryCompareUnsupported()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $expression = $this->createLogicalExpression('EQUAL_TO');
+
+        $subject->expects($this->once())
+                ->method('_isWpQueryCompareExpressionSupported')
+                ->with($expression)
+                ->willReturn(false);
+
+        $this->setExpectedException('InvalidArgumentException');
+
+        $reflect->_buildWpQueryCompare($expression);
+    }
+
+    /**
+     * Tests the WP Query comparison build method to assert whether an exception is thrown if the comparison key could
+     * not be determined.
+     *
+     * @since [*next-version*]
+     */
+    public function testBuildWpQueryCompareNoKey()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $expression = $this->createLogicalExpression('EQUAL_TO');
+
+        $subject->expects($this->once())
+                ->method('_isWpQueryCompareExpressionSupported')
+                ->with($expression)
+                ->willReturn(true);
+        $subject->expects($this->once())
+                ->method('_getWpQueryCompareKey')
+                ->with($expression)
+                ->willThrowException(new InvalidArgumentException());
+
+        $this->setExpectedException('InvalidArgumentException');
+
+        $reflect->_buildWpQueryCompare($expression);
+    }
+
+    /**
+     * Tests the WP Query comparison build method to assert whether an exception is thrown if the comparison value
+     * could not be determined.
+     *
+     * @since [*next-version*]
+     */
+    public function testBuildWpQueryCompareNoValue()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $expression = $this->createLogicalExpression('EQUAL_TO');
+
+        $subject->expects($this->once())
+                ->method('_isWpQueryCompareExpressionSupported')
+                ->with($expression)
+                ->willReturn(true);
+        $subject->expects($this->once())
+                ->method('_getWpQueryCompareValue')
+                ->with($expression)
+                ->willThrowException(new InvalidArgumentException());
+
+        $this->setExpectedException('InvalidArgumentException');
+
+        $reflect->_buildWpQueryCompare($expression);
+    }
+}
